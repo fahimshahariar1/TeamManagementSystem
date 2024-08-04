85a07cf160b9a17be78982388abe0a7919bc24b7
Fahim <fahimshahariar.fs@gmail.com>
2024-08-04 19:51:55 +0600

    part 2

--- TeamManagementSystem/Program.cs
+++ TeamManagementSystem/Program.cs
@@ -12,20 +12,20 @@ namespace TeamManagementSystem
         class Person
         {
             public string name { get; protected set; }
-            public int age { get; protected set; }
+            public int id { get; protected set; }
 
-            public Person(String name, int age)
+            public Person(String name, int id)
             {
 
                 this.name = name;
-                this.age = age;
+                this.id = id;
 
             }
 
             public String returnDetails()
             {
 
-                return name + " - " + age;
+                return name + " - " + id;
  
             }
 
@@ -33,9 +33,9 @@ namespace TeamManagementSystem
             {
                 this.name = name;
             }
-            public void setAge(int age)
+            public void setId(int id)
             {
-                this.age = age;
+                this.id = id;
             }
 
         }
@@ -47,8 +47,10 @@ namespace TeamManagementSystem
 
             public Manager() 
             {
-            
-                people = new List<Person>();
+
+                people = new List<Person>() { };
+
+
                 printMenu();
 
             }
@@ -71,7 +73,29 @@ namespace TeamManagementSystem
                 if (int.TryParse(Console.ReadLine(), out int menuOption))
 
                 {
-                    Console.WriteLine(menuOption);
+                    if (menuOption == 1)
+                    {
+                        PrintAll();
+                    }
+                    else if (menuOption == 2)
+                    {
+                        AddMember();
+                    }
+                    else if(menuOption == 3)
+                    {
+                        EditMember();
+                    }
+                    else if (menuOption == 4)
+                    {
+                        SearchMemebr();
+                    }
+                    else if (menuOption == 5)
+                    {
+                        RemoveMember();
+                    }
+
+                    printMenu();
+                    
                 }
                 else {
 
@@ -86,6 +110,123 @@ namespace TeamManagementSystem
             }
 
 
+            public void PrintAll()
+            {
+
+
+                StartOption("All Integration Team Members");
+
+                if (people.Count == 0)
+                {
+                    Console.WriteLine("No Members added, Use option 2 to create member");
+                }
+
+                for (int i = 0; i < people.Count; i++)
+                {
+                    Console.WriteLine(i + 1 + ". " + people[i].returnDetails());
+                }
+
+                FinishedOption();
+            }
+             public void AddMember()
+            {
+                StartOption("Add new member to the Integration Team");
+
+
+                try
+                {
+                    Console.Write("Enter a Name: ");
+                    String nameInput = Console.ReadLine();
+
+                    Console.Write("Enter an Ding ID: ");
+                    int idInput = Convert.ToInt32(Console.ReadLine());
+
+
+                    if (!String.IsNullOrEmpty(nameInput))
+                    {
+                        if (idInput > 0 && idInput <= 9999)
+                        {
+                            
+                            people.Add(new Person(nameInput, idInput));
+
+                            Console.WriteLine(Environment.NewLine+"Added Successfully");
+
+                            FinishedOption();
+
+                        }
+                        else
+                        {
+                           
+                            OutputMessage(Environment.NewLine + "Id must be 0 - 9999" + Environment.NewLine);
+                            AddMember();
+                        }
+                    }
+                    else
+                    {
+
+                        OutputMessage(Environment.NewLine  + "Name field can not be empty." + Environment.NewLine);
+
+
+                    }
+
+
+
+                }
+                catch (Exception)
+                {
+                    OutputMessage(Environment.NewLine + "Something Went Wrong" + Environment.NewLine);
+                    
+                    AddMember();
+
+                }
+
+
+
+            }
+
+            public void EditMember()
+            {
+                StartOption("Edit a Member");
+
+                FinishedOption();
+            }
+
+            public void SearchMemebr()
+            {
+                StartOption("Search Member");
+
+                FinishedOption();
+            }
+            public void RemoveMember()
+            {
+                StartOption("Remove a Member");
+
+                FinishedOption();
+            }
+
+            public void FinishedOption () {
+
+                Console.WriteLine();
+                Console.WriteLine("Press <Enter> to return back to the menu.");
+                Console.ReadLine();
+                Console.Clear();
+
+            }
+
+            public void StartOption(String message)
+            {
+
+                Console.Clear();
+                Console.WriteLine(message + Environment.NewLine);
+
+            }
+
+            public void OutputMessage(String message)
+            {
+                Console.WriteLine(message + Environment.NewLine + "Press <Enter> to return back to the menu.");
+                Console.ReadLine ();
+                Console.Clear();
+            }
         }
 
 
